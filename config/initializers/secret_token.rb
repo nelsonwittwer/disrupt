21d08f50b2d1774f4686ae43876de9dfd38366ae
Nelson Wittwer <nelsonwittwer@gmail.com>
2012-05-03 21:01:43 -0600

    inital commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Disrupt::Application.config.secret_token = '251b6d7248c7616552f340a8e0ad43443e2ad247cccc993713436035c7c6410ef282c85776a7704a472cc5fbab0562b341108ab7138cd19f93a5c6a848cf981e'
