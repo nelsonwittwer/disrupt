523f5d0e0b3797fb402a4ff13df58a9ad52ed989
Nelson Wittwer <nelsonwittwer@gmail.com>
2012-05-10 22:27:32 -0600

    delete garbage

--- app/models/market.rb
+++
@@ -1,4 +0,0 @@
-class Market < ActiveRecord::Base
-  attr_accessible :name
-  has_and_belongs_to_many :startups
-end
--- db/migrate/20120506035520_create_markets.rb
+++
@@ -1,9 +0,0 @@
-class CreateMarkets < ActiveRecord::Migration
-  def change
-    create_table :markets do |t|
-      t.string :name
-
-      t.timestamps
-    end
-  end
-end
