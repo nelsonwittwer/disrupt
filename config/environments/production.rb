473c7f901bff99be70791c6c7a925dbf7aa65099
Nelson <nelsonwittwer@gmail.com>
2012-08-25 09:40:28 -0600

    take 2

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,9 @@
 Disrupt::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
 
+  #Devise Mailer
+
+  config.action_mailer.default_url_options = { :host => 'disruptage.com' }
   # Code is not reloaded between requests
   config.cache_classes = true
 
