3555e56053866b8afeacd19c94588c1012391a8d
Nelson <nelsonwittwer@gmail.com>
2012-08-29 11:27:46 -0600

    s3

--- app/models/startup.rb
+++ app/models/startup.rb
@@ -25,20 +25,20 @@ class Startup < ActiveRecord::Base
   has_many :tags, :through => :taggings
 
 
-  has_attached_file :screenshot#,
-  # :storage => :s3,
-  #  :s3_credentials => {
-  #   :access_key_id => ENV['S3_KEY'],
-  #   :secret_access_key => ENV['S3_SECRET']},
-  # :bucket => ENV['S3_BUCKET'],  
-  #  :path => "/images/screenshots/:filename"
-  has_attached_file :startup_logo#,
-  # :storage => :s3,
-  #  :s3_credentials => {
-  #   :access_key_id => ENV['S3_KEY'],
-  #   :secret_access_key => ENV['S3_SECRET']},
-  # :bucket => ENV['S3_BUCKET'],
-  #   :path => "/images/logos/:filename"
+  has_attached_file :screenshot,
+  :storage => :s3,
+   :s3_credentials => {
+    :access_key_id => ENV['S3_KEY'],
+    :secret_access_key => ENV['S3_SECRET']},
+  :bucket => ENV['S3_BUCKET'],  
+   :path => "/images/screenshots/:filename"
+  has_attached_file :startup_logo,
+  :storage => :s3,
+   :s3_credentials => {
+    :access_key_id => ENV['S3_KEY'],
+    :secret_access_key => ENV['S3_SECRET']},
+  :bucket => ENV['S3_BUCKET'],
+    :path => "/images/logos/:filename"
  
   
 
